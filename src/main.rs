2d14b677f3a5c6c063aecec6900f91b66030877c
Blaine Price <1wbprice@gmail.com>
2019-08-06 17:57:26 -0400

    Ai place ships (#9)
    * stash.
    
    * Write some logic for determining if a ship placement is valid.
    
    * The logic looks good, but chasing down compile errors
    
    * vomit
    
    * potato
    
    * Theres's a corner case where the long boat can't be placed in the middle of the board.
    
    * this is not quite working yet. I need code to execute when both origin and heading are correct.
    
    * This is working decently, but it would be better if the ships weren't bunched up as much.
    
    * Documentation
    
    * Rust format

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::io::{stdin, stdout, Stdout, Stdin, Write};
+use rand::{
+    distributions::{Distribution, Standard},
+    seq::SliceRandom,
+    thread_rng, Rng,
+};
+use std::io::{stdin, stdout, Stdin, Stdout, Write};
 use termion::cursor::Goto;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
 use termion::{color, style};
+
 
 
 #[derive(Clone, Copy)]
@@ -23,7 +29,7 @@ enum Mode {
     Title,
     Game,
     Endscreen,
-    Quit
+    Quit,
 }
 
 #[derive(Clone, Copy)]
@@ -32,7 +38,7 @@ struct Game {
     red_score: Scores,
     turn: Faction,
     origin: Coordinates,
-    mode: Mode
+    mode: Mode,
 }
 
 impl Game {
@@ -42,7 +48,7 @@ impl Game {
             red_score: Scores::new(),
             turn: Faction::Blue,
             origin,
-            mode: Mode::Title
+            mode: Mode::Title,
         }
     }
 
@@ -97,15 +103,15 @@ impl Game {
             Mode::Title => {
                 game.mode = Mode::Title;
                 game
-            },
+            }
             Mode::Game => {
                 game.mode = Mode::Game;
                 game
-            },
+            }
             Mode::Endscreen => {
                 game.mode = Mode::Endscreen;
                 game
-            },
+            }
             Mode::Quit => {
                 game.mode = Mode::Quit;
                 game
@@ -152,7 +158,13 @@ enum Faction {
     Blue,
 }
 
-#[derive(Debug, Copy, Clone)]
+impl Default for Faction {
+    fn default() -> Self {
+        Faction::Red
+    }
+}
+
+#[derive(Debug, Copy, Clone, Default)]
 struct Coordinates {
     x: u16,
     y: u16,
@@ -173,10 +185,10 @@ struct Attack<'a> {
 }
 
 impl<'a> Attack<'a> {
-    fn new(coordinates: Coordinates, board: &'a Board, ships: Vec<Ship>) -> Attack<'a> {
+    fn new(coordinates: Coordinates, board: &'a Board, ships: &'a Vec<Ship>) -> Attack<'a> {
         let mut hit = false;
-        for ship in ships {
-            for segment in ship.segments {
+        for ship in ships.into_iter() {
+            for segment in ship.segments.iter() {
                 if segment.coordinates.x == coordinates.x && segment.coordinates.y == coordinates.y
                 {
                     hit = true;
@@ -233,12 +245,30 @@ enum AttackResults {
     Miss,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 enum Heading {
     North,
     East,
     West,
     South,
+}
+
+impl Default for Heading {
+    fn default() -> Self {
+        Heading::North
+    }
+}
+
+impl Distribution<Heading> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Heading {
+        match rng.gen_range(0, 3) {
+            0 => Heading::North,
+            1 => Heading::East,
+            2 => Heading::West,
+            3 => Heading::South,
+            _ => Heading::North,
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -326,7 +356,7 @@ impl<'a> Cursor<'a> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Board {
     faction: Faction,
     origin: Coordinates,
@@ -394,10 +424,7 @@ struct Label {
 
 impl Label {
     fn new(origin: Coordinates, content: String) -> Label {
-        Label {
-            origin,
-            content,
-        }
+        Label { origin, content }
     }
 
     fn render(&self, stdout: &mut RawTerminal<Stdout>) {
@@ -413,13 +440,14 @@ impl Label {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct Ship<'a> {
     origin: Coordinates,
     board: &'a Board,
     heading: Heading,
     segments: Vec<ShipSegment>,
 }
+
 
 impl<'a> Ship<'a> {
     fn new(origin: Coordinates, board: &'a Board, heading: Heading, length: u16) -> Ship {
@@ -454,8 +482,8 @@ impl<'a> Ship<'a> {
         }
     }
 
-    fn render(self, stdout: &mut RawTerminal<Stdout>) {
-        for segment in self.segments {
+    fn render(&self, stdout: &mut RawTerminal<Stdout>) {
+        for segment in self.segments.iter() {
             let board_coords = translate_game_coords_to_board_coords(segment.coordinates);
             let screen_coords = Coordinates {
                 x: board_coords.x + self.board.origin.x,
@@ -474,7 +502,7 @@ impl<'a> Ship<'a> {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 struct ShipSegment {
     coordinates: Coordinates,
 }
@@ -485,6 +513,129 @@ impl ShipSegment {
     }
 }
 
+fn auto_select_origin(board: &Board) -> Coordinates {
+    let mut rng = rand::thread_rng();
+    // Randomly choose origin
+    Coordinates {
+        x: rng.gen_range(0, board.width),
+        y: rng.gen_range(0, board.height),
+    }
+}
+
+fn is_legal_heading(origin: Coordinates, heading: Heading, length: u16) -> bool {
+    match heading {
+        Heading::North => {
+            // There should be enough room to place the ship heading north
+            if origin.y >= length {
+                true
+            } else {
+                false
+            }
+        }
+        Heading::South => {
+            // There should be enough room to place the ship heading south
+            if 8 - origin.y >= length {
+                true
+            } else {
+                false
+            }
+        }
+        Heading::West => {
+            // There should be enough room to place the ship heading west
+            if origin.x >= length {
+                true
+            } else {
+                false
+            }
+        }
+        Heading::East => {
+            // There should be enough room to place the ship heading east
+            if 8 - origin.x >= length {
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
+
+fn is_ship_at_coordinates(ships: &Vec<Ship>, coordinates: Coordinates) -> bool {
+    let mut result = false;
+    for ship in ships {
+        for segment in ship.segments.iter() {
+            if coordinates.x == segment.coordinates.x && coordinates.y == segment.coordinates.y {
+                result = true;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+fn is_legal_ship_placement(ships: &Vec<Ship>, new_ship: Ship) -> bool {
+    let mut result = true;
+    for ship in ships {
+        // If a ship segement belonging to new_ship is also in ship
+        // the ship placement is not legal
+        for new_segment in new_ship.segments.iter() {
+            for segment in ship.segments.iter() {
+                if new_segment.coordinates.x == segment.coordinates.x
+                    && new_segment.coordinates.y == segment.coordinates.y
+                {
+                    result = false;
+                    break;
+                }
+            }
+        }
+    }
+    result
+}
+
+fn autocreate_ship<'a>(ships: &Vec<Ship>, board: &'a Board, length: u16) -> Ship<'a> {
+    loop {
+        // Create an origin
+        // Any origin on the board is legal as long as there's not already a ship there
+        let mut origin = auto_select_origin(board);
+        let mut origin_is_legal = !is_ship_at_coordinates(ships, origin);
+
+        // Select a heading
+        // Any heading that doesn't lead the ship off the board is valid
+        let mut heading: Heading = rand::random();
+        let mut heading_is_legal = is_legal_heading(origin, heading, length);
+
+        // If origin isn't legal, pick another one randomly until it is
+        while !origin_is_legal {
+            origin = auto_select_origin(board);
+            origin_is_legal = is_ship_at_coordinates(ships, origin);
+        }
+
+        // Iterate through possible headings to find a legal heading
+        let mut random = thread_rng();
+        let mut headings = vec![Heading::North, Heading::East, Heading::West, Heading::South];
+        headings.shuffle(&mut random);
+
+        for h in headings {
+            if is_legal_heading(origin, h, length) {
+                heading = h;
+                heading_is_legal = true;
+                break;
+            }
+        }
+
+        // If both heading and origin aren't good, we can't continue.
+        // Try again.
+        if origin_is_legal && heading_is_legal {
+            // Create ship with legal origin and legal heading
+            let ship = Ship::new(origin, board, heading, length);
+            // If the newly created ship doesn't collide with existing ships,
+            // it's legal.
+            // Otherwise, find a new origin and heading and try again.
+            if is_legal_ship_placement(ships, ship) {
+                return Ship::new(origin, board, heading, length);
+            }
+        }
+    }
+}
 
 fn main() {
     let mut game = Game::new(Coordinates { x: 38, y: 2 });
@@ -506,18 +657,11 @@ fn main() {
                 .unwrap();
 
                 // Create entities
-                let title = Label::new(
-                    Coordinates { x: 1, y: 1},
-                    "Rustbuckets".to_string()
-                );
-                let instructions = Label::new(
-                    Coordinates { x: 1, y: 2},
-                    "Press F to start".to_string()
-                );
-                let quit_instructions = Label::new(
-                    Coordinates { x: 1, y: 2},
-                    "Press F to start".to_string()
-                );
+                let title = Label::new(Coordinates { x: 1, y: 1 }, "Rustbuckets".to_string());
+                let instructions =
+                    Label::new(Coordinates { x: 1, y: 2 }, "Press F to start".to_string());
+                let quit_instructions =
+                    Label::new(Coordinates { x: 1, y: 2 }, "Press F to start".to_string());
 
                 // Initial render
                 title.render(&mut stdout);
@@ -530,11 +674,11 @@ fn main() {
                         Key::Char('q') => {
                             game = game.toggle_mode(Mode::Quit);
                             break;
-                        },
+                        }
                         Key::Char('f') => {
                             game = game.toggle_mode(Mode::Game);
                             break;
-                        },
+                        }
                         _ => {}
                     }
 
@@ -544,7 +688,7 @@ fn main() {
                     quit_instructions.render(&mut stdout);
                     stdout.flush().unwrap();
                 }
-            },
+            }
             Mode::Game => {
                 // Clear all
                 let mut stdout = stdout().into_raw_mode().unwrap();
@@ -565,46 +709,20 @@ fn main() {
                 let mut cursor = Cursor::new(Coordinates { x: 0, y: 0 }, &red_board);
                 let mut attacks: Vec<Attack> = Vec::new();
                 let mut ships: Vec<Ship> = Vec::new();
-                let title = Label::new(Coordinates { x: 1, y: 1}, "Rustbuckets v0.1.0".to_string());
+                let title =
+                    Label::new(Coordinates { x: 1, y: 1 }, "Rustbuckets v0.1.0".to_string());
 
-                // Some test ships
-                ships.push(Ship::new(
-                    Coordinates { x: 2, y: 0 },
-                    &red_board,
-                    Heading::South,
-                    3,
-                ));
-                ships.push(Ship::new(
-                    Coordinates { x: 3, y: 3 },
-                    &red_board,
-                    Heading::East,
-                    3,
-                ));
-                ships.push(Ship::new(
-                    Coordinates { x: 0, y: 7 },
-                    &red_board,
-                    Heading::North,
-                    5,
-                ));
-                ships.push(Ship::new(
-                    Coordinates { x: 7, y: 7 },
-                    &red_board,
-                    Heading::North,
-                    4,
-                ));
-                ships.push(Ship::new(
-                    Coordinates { x: 4, y: 6 },
-                    &red_board,
-                    Heading::West,
-                    2,
-                ));
+                // Put some ships in the red_board
+                for length in vec![2, 2, 3, 4, 5] {
+                    ships.push(autocreate_ship(&ships, &red_board, length));
+                }
 
                 // Initial render
                 red_board.render(&mut stdout);
                 blue_board.render(&mut stdout);
                 title.render(&mut stdout);
                 game.render(&mut stdout);
-                for ship in ships.clone() {
+                for ship in ships.iter() {
                     ship.render(&mut stdout);
                 }
                 for attack in attacks.clone() {
@@ -634,7 +752,7 @@ fn main() {
                             cursor = cursor.on_move(Heading::East);
                         }
                         Key::Char('f') => {
-                            let attack = Attack::new(cursor.coordinates, &red_board, ships.clone());
+                            let attack = Attack::new(cursor.coordinates, &red_board, &ships);
                             game = match attack.result {
                                 AttackResults::Hit => game.increment_hits(),
                                 AttackResults::Miss => game.increment_misses(),
@@ -654,7 +772,7 @@ fn main() {
                     blue_board.render(&mut stdout);
                     title.render(&mut stdout);
                     game.render(&mut stdout);
-                    for ship in ships.clone() {
+                    for ship in ships.iter() {
                         ship.render(&mut stdout);
                     }
                     for attack in attacks.clone() {
@@ -663,7 +781,7 @@ fn main() {
                     cursor.render(&mut stdout);
                     stdout.flush().unwrap();
                 }
-            },
+            }
             Mode::Endscreen => {
                 // Endscreen mode setup
                 let mut stdout = stdout().into_raw_mode().unwrap();
@@ -679,17 +797,12 @@ fn main() {
                 .unwrap();
 
                 // Create entities
-                let title = Label::new(
-                    Coordinates { x: 1, y: 1},
-                    "Game End".to_string()
-                );
-                let quit_instructions = Label::new(
-                    Coordinates { x: 1, y: 2},
-                    "Press Q to quit".to_string()
-                );
+                let title = Label::new(Coordinates { x: 1, y: 1 }, "Game End".to_string());
+                let quit_instructions =
+                    Label::new(Coordinates { x: 1, y: 2 }, "Press Q to quit".to_string());
                 let replay_instructions = Label::new(
-                    Coordinates { x: 1, y: 3},
-                    "Press F to play again".to_string()
+                    Coordinates { x: 1, y: 3 },
+                    "Press F to play again".to_string(),
                 );
 
                 // Initial render
@@ -703,7 +816,7 @@ fn main() {
                         Key::Char('q') => {
                             game = game.toggle_mode(Mode::Quit);
                             break;
-                        },
+                        }
                         Key::Char('f') => {
                             game = Game::new(Coordinates { x: 38, y: 2 });
                             game = game.toggle_mode(Mode::Game);
@@ -718,9 +831,9 @@ fn main() {
                     replay_instructions.render(&mut stdout);
                     stdout.flush().unwrap();
                 }
-            },
+            }
             Mode::Quit => {
-            let mut stdout = stdout().into_raw_mode().unwrap();
+                let mut stdout = stdout().into_raw_mode().unwrap();
                 write!(stdout, "{}", style::Reset).unwrap();
                 break;
             }
@@ -792,5 +905,103 @@ mod tests {
         assert_eq!(ship.segments[1].coordinates.y, 1);
         assert_eq!(ship.segments[2].coordinates.x, 0);
         assert_eq!(ship.segments[2].coordinates.y, 2);
+    }
+
+    #[test]
+    fn test_auto_select_origin() {
+        let board = Board::new(Faction::Blue, Coordinates { x: 1, y: 2 }, 8, 8);
+        let origin = auto_select_origin(&board);
+
+        assert!(origin.x <= 7);
+        assert!(origin.x >= 0);
+        assert!(origin.y >= 0);
+        assert!(origin.y <= 7);
+    }
+
+    #[test]
+    fn test_is_legal_heading_north_0_0() {
+        let origin = Coordinates { x: 0, y: 0 };
+        let result = is_legal_heading(origin, Heading::North, 2);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_is_legal_heading_north_0_2() {
+        let origin = Coordinates { x: 0, y: 2 };
+        let result = is_legal_heading(origin, Heading::North, 2);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_legal_heading_south_0_7() {
+        let origin = Coordinates { x: 0, y: 7 };
+        let result = is_legal_heading(origin, Heading::South, 2);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_is_legal_heading_south_0_5() {
+        let origin = Coordinates { x: 0, y: 5 };
+        let result = is_legal_heading(origin, Heading::South, 2);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_legal_heading_west_0_0() {
+        let origin = Coordinates { x: 0, y: 0 };
+        let result = is_legal_heading(origin, Heading::West, 2);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_is_legal_heading_west_0_2() {
+        let origin = Coordinates { x: 0, y: 2 };
+        let result = is_legal_heading(origin, Heading::West, 2);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_is_legal_ship_placement_empty_board() {
+        let board = Board::new(Faction::Blue, Coordinates { x: 0, y: 0 }, 8, 8);
+        let ships: Vec<Ship> = vec![];
+        let tentative_ship = Ship::new(Coordinates { x: 0, y: 1 }, &board, Heading::South, 2);
+        let result = is_legal_ship_placement(&ships, tentative_ship);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_legal_ship_placement_non_empty_board() {
+        let board = Board::new(Faction::Blue, Coordinates { x: 0, y: 0 }, 8, 8);
+        let ships: Vec<Ship> = vec![Ship::new(
+            Coordinates { x: 0, y: 1 },
+            &board,
+            Heading::South,
+            2,
+        )];
+        let tentative_ship = Ship::new(Coordinates { x: 0, y: 1 }, &board, Heading::South, 2);
+        let result = is_legal_ship_placement(&ships, tentative_ship);
+
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_autocreate_ship_empty_board() {
+        let board = Board::new(Faction::Blue, Coordinates { x: 0, y: 0 }, 8, 8);
+        let mut ships: Vec<Ship> = vec![];
+        let ship1 = autocreate_ship(&ships, &board, 2);
+        ships.push(ship1);
+        assert_eq!(ships.len(), 1);
+    }
+
+    #[test]
+    fn test_autocreate_ship_5_ships() {
+        let board = Board::new(Faction::Blue, Coordinates { x: 0, y: 0 }, 8, 8);
+        let mut ships: Vec<Ship> = vec![];
+
+        for length in vec![2, 2, 3, 4, 5] {
+            let ship = autocreate_ship(&ships, &board, length);
+            ships.push(ship);
+        }
+        assert_eq!(ships.len(), 5);
     }
 }
